27390b58baa41de4ea6dfa524e4ba26d1a7cad79
Peter Brundage <peterbrundinator@gmail.com>
2019-08-16 19:46:46 -0400

    lol some stuff happen idk k lmao xD

--- SPI-I2C-PWM Excersize/Code/M1/M1_-_R2R_and_Shift_Register_test/M1_-_R2R_and_Shift_Register_test.ino
+++ SPI-I2C-PWM Excersize/Code/M1/M1_-_R2R_and_Shift_Register_test/M1_-_R2R_and_Shift_Register_test.ino
@@ -1,41 +1,41 @@
 #include <ShiftregM1.h>
 ShiftregM1 dorito;
 
-void setup(){
-  pinMode(A3,INPUT);
-  pinMode(A2,INPUT);
-  pinMode(A1,INPUT);
+void setup() {
   dorito.clearBoard();
-   dorito.forwards(0xAA);
+  pinMode(A3, INPUT);
+   pinMode(A2,INPUT);
+   pinMode(A1,INPUT);
+     pinMode(A0,INPUT);
+  dorito.forwards(240);
   delay(1000);
   dorito.clearBoard();
+
+
+  dorito.forwards(15);
   delay(1000);
-   dorito.forwards(0xF0);
   dorito.clearBoard();
-delay(1000);
-  
-   dorito.forwards(0xF0);
-  dorito.clearBoard();
-  delay(1000);
-  }
-void loop(){
+}
+void loop() {
+
+   dorito.forwards(map(analogRead(A0), 0, 1023, 0, 255));
+    delay(1000);
+    dorito.clearBoard();
+  /*
+   
+
+
   dorito.forwards(map(analogRead(A3), 0, 1023, 0, 255));
-  delay(1000);
+  delay(500);
   dorito.clearBoard();
-  
-  dorito.forwards(map(analogRead(A2), 0, 1023, 0, 255));
-  delay(1000);
-  dorito.clearBoard();
- 
-  
-  dorito.forwards(map(analogRead(A1), 0, 1023, 0, 255));
-  delay(1000);
-  dorito.clearBoard();
+    dorito.forwards(map(analogRead(A1), 0, 1023, 0, 255));
+    delay(1000);
+    dorito.clearBoard();
+  */
 
-  
- }
+}
 
- 
+
 
 
 
