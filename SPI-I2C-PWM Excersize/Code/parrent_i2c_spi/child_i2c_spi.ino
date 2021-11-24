bcf9a6f45ac3e024eef232380d5f0acedc9e3409
Peter Brundage <peterbrundinator@gmail.com>
2021-11-23 22:49:16 -0500

    included direct port i/o manipulation and led pin registers

--- SPI-I2C-PWM Excersize/Code/parrent_i2c_spi/child_i2c_spi.ino
+++ SPI-I2C-PWM Excersize/Code/parrent_i2c_spi/child_i2c_spi.ino
@@ -2,25 +2,45 @@
 #include <bitbang_shift_register.h>
 bitbang_shift_register child;
 
-#define CS_PIN 10
+//chip select pin
+//unused                  //PB0
+//unused                  //PB1
+#define CS_PIN_MASK 0x04  //PB2
+#define POCI_MASK 0x08    //PB3
+#define PICO_MASK 0x10    //PB4
+#define SCK_MASK 0x20     //PB5
+//unused                  //PB6
+//unused                  //PB7
+
+
+//led pins
+//unused                   //PD0-PD4 (shift register bit bang)
+#define SPI_LED_MASK 0x20  //PD5 
+#define i2c_LED_MASK 0x40  //PD6
+#define PWM_LED_MASK 0x80  //PD7
 
 void setup() {
 
+  //pinMode(CS_PIN, INPUT);
+  DDRB |= (CS_PIN_MASK) | (PICO_MASK);
+  DDRD |= (PWM_LED_MASK) | (i2c_LED_MASK)|(SPI_LED_MASK);
   child.shiftbyte(42);
   delay(1000);
-  pinMode(CS_PIN, INPUT);
-
-  /* Set MISO output, all others input */
-  pinMode(12, OUTPUT);
   /* Enable SPI */
   SPCR = (1 << SPE);
 
 }
 void loop() {
+  
+child_receive();
+}
+
+
+void child_receive(){
+  
   /* Wait for reception complete */
   while (!(SPSR & (1 << SPIF)))
     ;
   /* Return Data Register */
   child.shiftbyte( int( SPDR ) );
-
 }
