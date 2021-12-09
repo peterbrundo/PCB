f7f01beea1034297f43406e698b43113e1c7376f
peterbrundo <52147208+peterbrundo@users.noreply.github.com>
2021-12-09 12:25:36 -0500

    Create BOMChecker.bat
    messy way of checking part status

---
+++ BOMChecker.bat
@@ -0,0 +1,168 @@
+
+rem  messy way of pulling up URL links to check part status
+rem TIMEOUT /T 1 to prevent webpages from locking us out
+rem  mouser links don't seem to work  
+
+
+@echo off
+
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/molex/0430250210/9740782?WT.z_cid=sp_900_0310_buynow&s=N4IgTCBcDaIAwBYDMcwFZUEY4gLoF8g
+TIMEOUT /T 1
+start https://www.newark.com/molex/43025-0410/connector-rohs-compliant-yes/dp/41AC9481?ost=0430250410&CMP=AFC-MOLEX&krypto=qsj%2BO%2BhVLbtbEOfFz1zPsB1Lue%2F%2BwwLfSp9GvfMviedy6Ga3T%2FsImcOM9DQ2h23YqG9pqLJNMsYpRyoMAdPAXQ%3D%3D
+TIMEOUT /T 1
+start https://www.newark.com/molex/43025-1400/connector-housing-rcpt-14pos/dp/53R1250?ost=0430251400&CMP=AFC-MOLEX&krypto=2MJ9xhzBz9uNE8caShn%2FZHFVyP0FjAECnglbBtyUdHZ%2FMCOd4D3QkW2USzRuJTF%2Ba461Z096DUdrPDNL9VaLvQ%3D%3D
+TIMEOUT /T 1
+start https://www.digikey.com/short/bzmqpcdp
+TIMEOUT /T 1
+start https://www.mcmaster.com/91772A116/
+TIMEOUT /T 1
+start https://www.mcmaster.com/92510A692/
+TIMEOUT /T 1
+start https://www.mcmaster.com/92510A772/
+TIMEOUT /T 1
+start https://www.amazon.com/SunFounder-Serial-Module-Arduino-Mega2560/dp/B07MTFDHXZ/ref=sr_1_1_sspa?dchild=1&keywords=20%2Bx%2B4%2Blcd%2Bi2c&qid=1587592603&sr=8-1-spons&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzODFURzlNRzYwM1JYJmVuY3J5cHRlZElkPUEwNzEwMjEzMlVWOThHUVY5QUtKUyZlbmNyeXB0ZWRBZElkPUEwNjg1NjIxM1BOMlNOR0xURExXSCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU&th=1
+TIMEOUT /T 1
+start https://www.newark.com/hammond/1591esbk/enclosure-multipurpose-abs-black/dp/87F2526?st=1591esbk
+TIMEOUT /T 1
+start https://www.newark.com/samtec/snt-100-bk-g/jumper-2way-2-54mm/dp/73J7699
+TIMEOUT /T 1
+start https://www.newark.com/adafruit/266/wire-gauge-28awg/dp/42X1200?ost=Premium+Female%2FFemale+Jumper+Wires%2C+40+x+6%22%28150mm%29&cfm=true&ddkey=https%3Aen-US%2FElement14_US%2Fsearch
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/b-f-fastener-supply/HNZ-440/5738
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/molex/0430250210/9740782?WT.z_cid=sp_900_0310_buynow&s=N4IgTCBcDaIAwBYDMcwFZUEY4gLoF8g
+TIMEOUT /T 1
+start https://www.newark.com/molex/43025-0410/connector-rohs-compliant-yes/dp/41AC9481?ost=0430250410&CMP=AFC-MOLEX&krypto=qsj%2BO%2BhVLbtbEOfFz1zPsB1Lue%2F%2BwwLfSp9GvfMviedy6Ga3T%2FsImcOM9DQ2h23YqG9pqLJNMsYpRyoMAdPAXQ%3D%3D
+TIMEOUT /T 1
+start https://www.newark.com/molex/43025-1400/connector-housing-rcpt-14pos/dp/53R1250?ost=0430251400&CMP=AFC-MOLEX&krypto=2MJ9xhzBz9uNE8caShn%2FZHFVyP0FjAECnglbBtyUdHZ%2FMCOd4D3QkW2USzRuJTF%2Ba461Z096DUdrPDNL9VaLvQ%3D%3D
+TIMEOUT /T 1
+start https://www.digikey.com/short/bzmqpcdp
+TIMEOUT /T 1
+start https://www.mcmaster.com/91772A116/
+TIMEOUT /T 1
+start https://www.mcmaster.com/92510A692/
+TIMEOUT /T 1
+start https://www.mcmaster.com/92510A772/
+TIMEOUT /T 1
+start https://www.amazon.com/SunFounder-Serial-Module-Arduino-Mega2560/dp/B07MTFDHXZ/ref=sr_1_1_sspa?dchild=1&keywords=20%2Bx%2B4%2Blcd%2Bi2c&qid=1587592603&sr=8-1-spons&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzODFURzlNRzYwM1JYJmVuY3J5cHRlZElkPUEwNzEwMjEzMlVWOThHUVY5QUtKUyZlbmNyeXB0ZWRBZElkPUEwNjg1NjIxM1BOMlNOR0xURExXSCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU&th=1
+TIMEOUT /T 1
+start https://www.newark.com/hammond/1591esbk/enclosure-multipurpose-abs-black/dp/87F2526?st=1591esbk
+TIMEOUT /T 1
+start https://www.newark.com/samtec/snt-100-bk-g/jumper-2way-2-54mm/dp/73J7699
+TIMEOUT /T 1
+start https://www.newark.com/adafruit/266/wire-gauge-28awg/dp/42X1200?ost=Premium+Female%2FFemale+Jumper+Wires%2C+40+x+6%22%28150mm%29&cfm=true&ddkey=https%3Aen-US%2FElement14_US%2Fsearch
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/b-f-fastener-supply/HNZ-440/5738
+TIMEOUT /T 1
+start https://www.mouser.com/ProductDetail/Molex/43045-0216?qs=uJX40y%252BSWqhVKOe7L4WCAw%3D%3D
+TIMEOUT /T 1
+start https://www.mouser.com/ProductDetail/Molex/43045-1416?qs=HSGlLeu24BOERYIV%2FA7ohg%3D%3D
+TIMEOUT /T 1
+start https://www.ckswitches.com/search-distributor-stock/?mode=3&partnum1=7105J11CQE22
+TIMEOUT /T 1
+start https://www.mouser.com/ProductDetail/CK/7105J11CQE22?qs=vdMu3VPLtC7L7IUg0HsvPA%3D%3D
+TIMEOUT /T 1
+start https://www.ckswitches.com/search-distributor-stock/?mode=3&partnum1=7105J11CQE22
+TIMEOUT /T 1
+start https://www.mouser.com/ProductDetail/CK/7201J11CQE22?qs=%2Fha2pyFadujza9VyVfQwQUig2D7wB1kOCWmbywQ2GnBRJeX5eAKS4g%3D%3D
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/amphenol-icc-fci/61729-0010BLF/1001353
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/molex/0015910200/614780?s=N4IgTCBcDaIIwFYCccAMZWpAXQL5A
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/molex/0430450416/3310186?s=N4IgTCBcDaICwGYAMcCsKCMA2EBdAvkA
+TIMEOUT /T 1
+start https://www.mouser.com/ProductDetail/Molex/43045-1416?qs=HSGlLeu24BOERYIV%2FA7ohg%3D%3D
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/molex/0878980454/4554964
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/te-connectivity-amp-connectors/1-338068-0/1860403?s=N4IgTCBcDaIIwFoDMSAcAGAbKh6QF0BfIA
+TIMEOUT /T 1
+start https://www.newark.com/avx/08053c104kat2a/ceramic-capacitor-0-1uf-25v-x7r/dp/96M1381
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/bourns-inc/CR0805-FX-1000ELF/3740946
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/vishay-dale/CRCW080510K0JNEA/1175222
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/bourns-inc/CV201210-100K/1969710?s=N4IgTCBcDaIMIDUwAYCMZXILSeQaRAF0BfIA
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/txc-corporation/9B-16-000MBBK-B/3522089
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/txc-corporation/7V-16-000MDDJ-T/11587595
+TIMEOUT /T 1
+start https://www.newark.com/panasonic/erj-6enf1004v/res-thick-film-1m-1-0-125w-0805/dp/64R5372?st=ERJ-6ENF1004V
+TIMEOUT /T 1
+start https://www.newark.com/vishay/crcw08051k00fkeb/res-thick-film-1k-1-0-125w-0805/dp/26R4044
+TIMEOUT /T 1
+start https://www.newark.com/taiyo-yuden/umk212b7105kg-t/ceramic-capacitor-1uf-50v-x7r/dp/47T1712?st=1uf%20capacitor%200805
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/te-connectivity-alcoswitch-switches/FSM2JSMAATR/1201469?s=N4IgTCBcDaIGIGUCyYBSyCCGAqAlEAugL5A
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/bourns-inc/CR0805-JW-220ELF/3785269
+TIMEOUT /T 1
+start https://www.digikey.com/short/13v77jhj
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/bourns-inc/CR0603-JW-472ELF/3593210
+TIMEOUT /T 1
+start https://www.mouser.com/ProductDetail/Panasonic/EEE-1EA470WP?qs=%2Fha2pyFadug8Pl57h5%252BWRwGrTZPSQOrs3zQttOkUHPdWIk0Yx00lwg%3D%3D&utm_source=octopart&utm_medium=aggregator&utm_campaign=667-EEE-1EA470WP&utm_content=Panasonic
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/bourns-inc/CR0805-JW-511ELF/3785336
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/avx-corporation/06033A561JAT2A/1599276
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/bourns-inc/CR0805-FX-5101ELF/3784995
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/microchip-technology/ATMEGA16U2-MUR/2238234?s=N4IgTCBcDaIIIBUCyBRA4nAjANgKpgFolcAlEAXQF8g
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/microchip-technology/ATMEGA2560-16AU/735455
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/bourns-inc/CG0603MLC-05E/2538084
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/murata-electronics/CSTNE16M0V530000R0/8747729?s=N4IgTCBcDaIMIGUAqA5AogRgGwFkAMAagKwDMe5eASniALoC%2BQA
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/amphenol-icc-fci/LD09P33E4GX00LF/4997290
+TIMEOUT /T 1
+start https://www.newark.com/kingbright/kpt-1608sgc/led-green-12mcd-568nm/dp/93T1594?ost=kpt-1608sgc
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/amphenol-icc-fci/95278-101A06LF/1487966?s=N4IgTCBcDaIJwFYwHYAcBaAjABkwQWwDYAZAMRAF0BfIA
+TIMEOUT /T 1
+start https://www.newark.com/stmicroelectronics/ld1117s50ctr/ldo-voltage-regulator-5v-smd/dp/58M7943?st=ld1117s50ctr
+TIMEOUT /T 1
+start https://www.newark.com/texas-instruments/lmv358idgkr/op-amp-low-voltage-1mhz-1v-us/dp/33AH4017?CMP=AFC-OP
+TIMEOUT /T 1
+start https://www.newark.com/texas-instruments/lp2985-33dbvr/ldo-fixed-3-3v-0-15a-sot-23-5/dp/28AH3693?CMP=AFC-OP
+TIMEOUT /T 1
+start https://americas.rsdelivers.com/product/microchip/mcp2515-i-so/microchip-mcp2515-i-so-can-controller-1mbps-can/6283548/?cm_mmc=aff2-_-us-_-octopart-_-Instock6283548
+TIMEOUT /T 1
+start https://www.digikey.com/short/hvb4qwcp
+TIMEOUT /T 1
+start https://www.newark.com/bourns/mf-msmf010-2/fuse-resettable-ptc-60v-100ma/dp/87W6083?gclid=Cj0KCQjw1PSDBhDbARIsAPeTqrdECwxDHgJeii6h_swBQSm6RShXEyADH5HNlEix_s2zAW5ucYtJSl0aAkLkEALw_wcB&s_kwcid=AL!8472!3!487393244148!p!!g!!mf-msmf010-2&mckv=s_dc|pcrid|487393244148|plid||kword|mf-msmf010-2|match|p|slid||product||pgrid|113807124205|ptaid|kwd-10790063397|&CMP=KNC-GUSA-SKU-MDC-POWER_SUPPLIES_CIRCUIT_PROTECTION
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/bourns-inc/MF-MSMF050-2/662816?s=N4IgTCBcDaILIDEC0cDKiAMBWDSIF0BfIA
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/bourns-inc/mh2029-300y/2563323
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/diodes-incorporated/MMSZ5230B-7-F/814370
+TIMEOUT /T 1
+start https://www.newark.com/nexperia/pesd1can-ux/esd-diode-aec-q101-24v-9-3pf-sot/dp/46Y2401?st=PESD1CAN-U
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/nexperia-usa-inc/PMV48XPAR/5170057
+TIMEOUT /T 1
+start https://www.newark.com/on-semiconductor/s1m/standard-diode-1a-1kv-do-214ac/dp/58K2390
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/osram-opto-semiconductors-inc/LY-R976-PS-36/1228052
+TIMEOUT /T 1
+start https://www.digikey.com/short/j804djfr
+TIMEOUT /T 1
+start https://www.digikey.com/en/products/detail/bourns-inc/CR0805-FX-1200ELF/3784599
+TIMEOUT /T 1
+start https://www.mouser.com/ProductDetail/Littelfuse/01530007Z?qs=gu7KAQ731URFnx4B9aYM4g%3D%3D
+TIMEOUT /T 1
+start https://www.mouser.com/ProductDetail/TE-Connectivity/208223-9?qs=d9SXFZzHO%2FZcV%2FKjvZOOnw%3D%3D
+TIMEOUT /T 1
+start https://www.mouser.com/ProductDetail/Molex/43045-0216?qs=uJX40y%252BSWqhVKOe7L4WCAw%3D%3D
+TIMEOUT /T 1
+start https://www.mouser.com/ProductDetail/Molex/43045-1416?qs=HSGlLeu24BOERYIV%2FA7ohg%3D%3D
+TIMEOUT /T 1
+start https://www.newark.com/littelfuse/0297002-wxnv/fuse-mini-blade-2a/dp/98K4443?st=2%20amp%20mini%20fuse%20littlefuse
