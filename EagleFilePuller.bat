c0b7e9e0470fa483cd455c201b68899df93b7903
peterbrundo <52147208+peterbrundo@users.noreply.github.com>
2021-12-09 12:29:43 -0500

    Create EagleFilePuller.bat
    Made this batch file to pull files from different directories into my local github repository.
    
    Probably going to be most used with Eagle files
    
    Need to ad

---
+++ EagleFilePuller.bat
@@ -0,0 +1,5 @@
+rem to do:
+rem -> add Documents/Eagle/
+rem -> -> add invidiual eagle projects
+
+xcopy /s "C:\Users\PBRUNDA1\Documents\Batch file info\copyandpastetesting\application\applicationfile.txt" "C:\Users\PBRUNDA1\Documents\Batch file info\copyandpastetesting\githubroot" /Y
