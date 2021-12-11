484275615700581afef3fc8d5882b356aaf5f8d0
Peter Brundage <peterbrundinator@gmail.com>
2021-12-11 00:12:16 -0500

    updates to capacitor and crystal symbols, added a bat file to pull the library file

---
+++ Business Card project/Eagle Library/EagleFilePuller.bat
@@ -0,0 +1,3 @@
+
+
+xcopy /s "C:\Users\peter\AppData\Roaming\Eagle\lbr\adsk.eagle_library_14925180\Brundage Buisness Card.lbr" "C:\Users\peter\Documents\PCB\Business Card project\Eagle Library" /Y
--- EagleFilePuller.bat
+++ EagleFilePuller.bat
@@ -2,4 +2,4 @@ rem to do:
 rem -> add Documents/Eagle/
 rem -> -> add invidiual eagle projects
 
-xcopy /s "C:\Users\PBRUNDA1\Documents\Batch file info\copyandpastetesting\application\applicationfile.txt" "C:\Users\PBRUNDA1\Documents\Batch file info\copyandpastetesting\githubroot" /Y
+xcopy /s "C:\Users\peter\AppData\Roaming\Eagle\lbr\adsk.eagle_library_14925180\Brundage Buisness Card.lbr" "C:\Users\peter\Documents\PCB" /Y
